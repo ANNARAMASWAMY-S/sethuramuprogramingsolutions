e1ceb561f02733103828ca887e7eac1ea2b59035
ANNARAMASWAMY-S <annaramaswamy7@gmail.com>
2025-06-22 10:55:55 +0530

    Create LinearSearch.java

---
+++ week-1/DataStructuresandAlgorithms/Exercise-2/LinearSearch.java
@@ -0,0 +1,16 @@
+package product;
+
+
+
+public class LinearSearch {
+
+    public static Product linearSearch(Product[] products, String target) {
+        for (Product p : products) {
+            if (p.productName.equalsIgnoreCase(target)) {
+                return p;
+            }
+        }
+        return null;
+    }
+}
+
