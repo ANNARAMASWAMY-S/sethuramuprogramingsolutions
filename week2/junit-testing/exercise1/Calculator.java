709294fbf451faee011c73651e9fc2a01cac21d8
ANNARAMASWAMY-S <annaramaswamy7@gmail.com>
2025-06-29 18:12:26 +0530

    Create Calculator.java

---
+++ week2/junit-testing/exercise1/Calculator.java
@@ -0,0 +1,23 @@
+package Calculator;
+
+public class Calculator 
+{
+	public Calculator()
+	{}
+	public int add(int a, int b)
+	{
+		return(a+b);
+	}
+	
+	public int sub(int a, int b)
+	{
+		return(a-b);
+	}
+	
+	public int mul(int a, int b)
+	{
+		return(a*b);
+	}
+	
+	
+}
