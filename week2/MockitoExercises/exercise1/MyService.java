9dbcbe9c25079941497b708d192c0a53fb26ebf2
ANNARAMASWAMY-S <annaramaswamy7@gmail.com>
2025-06-29 18:28:04 +0530

    Create MyService.java

---
+++ week2/MockitoExercises/exercise1/MyService.java
@@ -0,0 +1,16 @@
+package ExternalAPI.MyService;
+
+import ExternalAPI.ExternalAPI;
+
+public class MyService 
+{
+	private ExternalAPI api;
+
+    public MyService(ExternalAPI api) {
+        this.api = api;
+    }
+
+    public String fetchData() {
+        return api.getData();
+    }
+}
