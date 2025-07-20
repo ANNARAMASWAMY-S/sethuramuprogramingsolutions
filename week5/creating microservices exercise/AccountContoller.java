56d527ea2d8dee3439161c74f1a18efca847ee32
ANNARAMASWAMY-S <annaramaswamy7@gmail.com>
2025-07-20 17:47:09 +0530

    Create AccountContoller.java

---
+++ week5/creating microservices exercise/AccountContoller.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController 
+{
+	@GetMapping("/accounts/{number}")
+	public String account(@PathVariable String number)
+	{
+		return("{ number: \"00987987973432\", type: \"savings\", balance: 234343 }");
+	}
+}
