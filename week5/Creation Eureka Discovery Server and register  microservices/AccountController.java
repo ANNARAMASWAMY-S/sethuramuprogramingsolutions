b23a1908d1e6cef63f40c5b70c9eec4627b973d0
ANNARAMASWAMY-S <annaramaswamy7@gmail.com>
2025-07-20 18:25:50 +0530

    Create AccountController.java

---
+++ week5/Creation Eureka Discovery Server and register  microservices/AccountController.java
@@ -0,0 +1,12 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+	@GetMapping("/accounts/{number}")
+	public String acccount(@PathVariable String number)
+	{
+		return("{ number: \"00987987973432\", type: \"savings\", balance: 234343 }");
+	}
+}
