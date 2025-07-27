df5db7bca9a59620bf7abfcdb58f5c00e5b856ce
ANNARAMASWAMY-S <annaramaswamy7@gmail.com>
2025-07-27 17:46:24 +0530

    Create Contact.js

---
+++ week6/exercise2/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Contact(){
+    return(
+        <>
+        <h3 style={{textAlign:'center'}}>Welcome to the Contact page of the student management portal</h3>
+        </>
+    );
+}
