8e29be6132653147cae37e67887296f7c602030a
ANNARAMASWAMY-S <annaramaswamy7@gmail.com>
2025-07-27 17:56:17 +0530

    Create App.js

---
+++ week6/exercise3/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import CalculateScore from './CalculateScore';
+
+function App() {
+  return (
+    <>
+    <CalculateScore Name={"Steeve"}
+      School={"DNV Public School"}
+      Total={284}
+      Goal={3}
+     />
+    </>
+  );
+}
+
+export default App;
