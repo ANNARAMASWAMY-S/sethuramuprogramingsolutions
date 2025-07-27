745c7d0602bd4e82c10a45ff8f9812da7758d8bc
ANNARAMASWAMY-S <annaramaswamy7@gmail.com>
2025-07-27 17:47:08 +0530

    Create Home.js

---
+++ week6/exercise2/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home()
+{
+    return(
+        <>
+            <h3 style={{textAlign:'center'}}>Welcome to the home page of the student Management portal</h3>
+        </>
+    );
+}
