b290a6d0224f5e0a1bfcd70d67e0da4350c364a6
ANNARAMASWAMY-S <annaramaswamy7@gmail.com>
2025-07-27 17:45:38 +0530

    Create About.js

---
+++ week6/exercise2/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function About(){
+    return(<>
+        <h3 style={{textAlign:'center'}}>Welcome to the About page of the student management portal</h3>
+    </>);
+}
+
+export default About;
